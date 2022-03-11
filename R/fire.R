aca976735c340c6b56f346d0ac4eada2ac2900b8
Tristan Thrasher <tristanthrasher@Tristans-MacBook-Pro.local>
2022-03-10 21:47:39 -0600

    Added FIREDAM dataset

---
+++ R/fire.R
@@ -0,0 +1,11 @@
+#' fire data set.
+#'
+#' A data set collected by a fire insurance company, includes a sample of 15 recent fires in a suburb.
+#'
+#' @format Data frame with 15 rows and 2 variables:
+#' \describe{
+#'  \item{DISTANCE}{X distance from fire station in miles}
+#'  \item{DAMAGE}{Y Fire Damage 1000s of dollars}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
